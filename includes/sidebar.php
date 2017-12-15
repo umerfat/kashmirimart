dcab97d6ef643f91607989248673e03ba718c9f5
vinod10 <vinodkumar@kwiqpick.com>
2017-12-15 11:15:47 +0530

    fixed toggle sidebar issue

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,22 +1,3 @@
-<!--<div class="category m-b-lg-50 ">-->
-<!--    <div class="heading m-b-lg-0">-->
-<!--        <h3 class="p-l-lg-20" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">-->
-<!--            <i class="fa fa-bars"></i>Caterory-->
-<!--        </h3>-->
-<!--    </div>-->
-<!--    <ul class="collapse in" id="collapseExample">-->
-<!--        <li class="active"><a href="#">Cat 1<i class="fa fa-chevron-right pull-right"></i></a></li>-->
-<!--        <li><a href="#">Cat 2<i class="fa fa-chevron-right pull-right"></i></a></li>-->
-<!--        <li><a href="#">Cat 3<i class="fa fa-chevron-right pull-right"></i></a></li>-->
-<!--        <li><a href="#">Cat 4<i class="fa fa-chevron-right pull-right"></i></a></li>-->
-<!--        <li><a href="#">Cat 4<i class="fa fa-chevron-right pull-right"></i></a></li>-->
-<!--    </ul>-->
-<!--</div>-->
-
-
-
-
-
 <div class="affix-sidebar">
     <div class="sidebar-nav">
         <div class="navbar navbar-default" role="navigation">
@@ -33,24 +14,24 @@
                 <h4 class="cat-text">Categories</h4>
                 <ul class="nav navbar-nav" id="sidenav01">
                     <li class="active">
-                        <a href="#" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">Cat Sub 1 <span class="caret pull-right"></span>
+                        <a href="javascript:void(0)" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">Cat Sub 1 <span class="caret pull-right"></span>
                         </a>
                         <div class="collapse" id="toggleDemo" style="height: 0px;">
                             <ul class="nav nav-list">
-                                <li><a href="#">Submenu1.1</a></li>
-                                <li><a href="#">Submenu1.2</a></li>
-                                <li><a href="#">Submenu1.3</a></li>
+                                <li><a href="item.php">Submenu1.1</a></li>
+                                <li><a href="item.php">Submenu1.2</a></li>
+                                <li><a href="item.php#">Submenu1.3</a></li>
                             </ul>
                         </div>
                     </li>
                     <li>
-                        <a href="#" data-toggle="collapse" data-target="#toggleDemo2" data-parent="#sidenav01" class="collapsed">Cat sub 2 <span class="caret pull-right"></span>
+                        <a href="javascript:void(0)" data-toggle="collapse" data-target="#toggleDemo2" data-parent="#sidenav01" class="collapsed">Cat sub 2 <span class="caret pull-right"></span>
                         </a>
                         <div class="collapse" id="toggleDemo2" style="height: 0px;">
                             <ul class="nav nav-list">
-                                <li><a href="#">Submenu2.1</a></li>
-                                <li><a href="#">Submenu2.2</a></li>
-                                <li><a href="#">Submenu2.3</a></li>
+                                <li><a href="item.php">Submenu2.1</a></li>
+                                <li><a href="item.php">Submenu2.2</a></li>
+                                <li><a href="item.php">Submenu2.3</a></li>
                             </ul>
                         </div>
                     </li>
